e5f46c6e6396a7cff4ea9d24029b1cefde0a9c9e
Paul Willis <perceptual.compensation@gmail.com>
2017-01-09 03:07:27 -0800

    Draft of classifier

--- Classifier.py
+++ Classifier.py
@@ -1,6 +1,28 @@
+import json
+import numpy as np
 
-import re
+with open("Results.json") as f:
+    data = json.load(f)
+with open("Sense counts.json") as f:
+    senseCounts = json.load(f)
+with open("Word senses.json") as f:
+    wordSenses = json.load(f)
 
-lemMatch = re.compile("lemma=(.+?) ")
-lxnMatch = re.compile("lexsn=(.+?) ")
+words = dict(zip(wordSenses.keys(), range(len(wordSenses.keys()))))
+senses = dict(zip(senseCounts.keys(), range(len(senseCounts.keys()))))
 
+sentMatrix = np.full((len(senses), len(words)), 0.1)
+paraMatrix = np.full((len(senses), len(words)), 0.1)
+
+for para in data:
+    paraWords = set()
+    paraSenses = set()
+    for sentence in para:
+        w = [words[i] for i in sentence["Words"]]
+        s = [senses[i] for i in sentence["Senses"]]
+        paraWords.update(w)
+        paraSenses.update(s)
+        for i in w:
+            sentMatrix[s, i] += 1
+    for i in paraWords:
+        paraMatrix[paraSenses, i] += 1
--- tableizer.py
+++ tableizer.py
@@ -1,10 +1,58 @@
+import re
+import json
 
-tblFile = open("Bag of words table", "a")
+paraMatch = re.compile("^<p ")
+sentMatch = re.compile("^<s ")
+wordMatch = re.compile("lemma=(.+?)[ >]")
+senseMatch = re.compile("lexsn=(.+?)[ >]")
+
+sentence = 0
+data = []
+senseCount = {}
+wordSenses = {}
 
 with open("All files", "rt") as inFile:
-    lines = inFile.read().split()
+    lines = inFile.read().split("\n")
+
+tblFile = open("Bag of words table", "wt")
 
 for line in lines:
-    
+    if paraMatch.search(line):
+        if data:
+            data[-1][-1]["Words"] = list(data[-1][-1]["Words"])
+            data[-1][-1]["Senses"] = list(data[-1][-1]["Senses"])
+        data.append([])
+    elif sentMatch.search(line):
+        sentence += 1
+        if data[-1]:
+            data[-1][-1]["Words"] = list(data[-1][-1]["Words"])
+            data[-1][-1]["Senses"] = list(data[-1][-1]["Senses"])
+        data[-1].append({"Words" : set(), "Senses" : set()})
+    elif wordMatch.search(line):
+        word = wordMatch.search(line).groups()[0]
+        senses = senseMatch.search(line).groups()[0].split(";")
+        if not wordSenses.get(word):
+            wordSenses[word] = {}
+        data[-1][-1]["Words"].add(word)
+        for sense in senses:
+            if not senseCount.get(sense):
+                senseCount[sense] = 0
+            senseCount[sense] += 1
+            if not wordSenses[word].get(sense):
+                wordSenses[word][sense] = 0
+            wordSenses[word][sense] += 1
+            data[-1][-1]["Senses"].add(sense)
+            tblFile.write("\t".join((str(len(data)), str(sentence), 
+                    word, sense)) + "\n")
 
 tblFile.close()
+
+data[-1][-1]["Words"] = list(data[-1][-1]["Words"])
+data[-1][-1]["Senses"] = list(data[-1][-1]["Senses"])
+
+with open("Results.json", "wt") as f:
+    f.write(json.dumps(data, sort_keys=True, indent=4))
+with open("Word senses.json", "wt") as f:
+    f.write(json.dumps(wordSenses, sort_keys=True, indent=4))
+with open("Sense counts.json", "wt") as f:
+    f.write(json.dumps(senseCount, sort_keys=True, indent=4))
